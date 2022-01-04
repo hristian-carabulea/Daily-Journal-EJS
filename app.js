8c36fc48bfc31d42e97264af5035b08e861b0ce2
hristian-carabulea <hristian.carabulea@outlook.com>
2022-01-05 00:22:23 +0100

    Refactor code

--- app.js
+++ app.js
@@ -9,42 +9,45 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
+let app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function (req, res) {
   let year = date("year");
-  // console.log(day);
-  res.render("home", { homeContent: homeStartingContent, copyrightYear: year }); // home.ejs in view
+  res.render("home", { homeContent: homeStartingContent, newPosts: posts, copyrightYear: year }); // home.ejs in view
 }); // end app.get("/", function(req, res)
 
 app.get("/about", function (req, res) {
   let year = date("year");
-  // console.log(day);
   res.render("about", { aboutContent: aboutContent, copyrightYear: year }); // home.ejs in view
 }); // end app.get("/", function(req, res) 
 
 app.get("/contact", function (req, res) {
   let year = date("year");
-  // console.log(day);
   res.render("contact", { contactContent: contactContent, copyrightYear: year }); // home.ejs in view
 }); // end app.get("/", function(req, res)
 
-app.get("/compose", function (req, res) {
+app.get("/compose", function (req, res){
   let year = date("year");
-  // console.log(day);
   res.render("compose", { copyrightYear: year }); // home.ejs in view
 }); // end app.get("/", function(req, res)
 
-app.post("/", function(req, res){
-  let publishNewItemTitle = req.body.newComposeItemTitle; 
-  console.log(publishNewItemTitle);
-  let publishNewItemBody = req.body.newComposeItemBody; 
-  console.log(publishNewItemBody);
+app.post("/compose", function(req, res){
+  // must have action="compose" in compose.ejs otherwise getting message: cannot post /
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  let year = date("year");
+  posts.push(post);
+  res.redirect("/");
+
 });
 
 app.listen(3000, function() {
